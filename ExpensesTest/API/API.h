f7b3cab0bc8849ec20f537b4bcca404d88fcc133
Luis Ascorbe <devlascorbe@gmail.com>
2020-05-07 16:32:47 +0200

    Create API and Storage frameworks

---
+++ ExpensesTest/API/API.h
@@ -0,0 +1,15 @@
+//
+//  API.h
+//  API
+//
+//  Created by Luis Ascorbe on 07/05/2020.
+//  Copyright © 2020 Luis Ascorbe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for API.
+FOUNDATION_EXPORT double APIVersionNumber;
+
+//! Project version string for API.
+FOUNDATION_EXPORT const unsigned char APIVersionString[];
---
+++ ExpensesTest/Storage/Storage.h
@@ -0,0 +1,15 @@
+//
+//  Storage.h
+//  Storage
+//
+//  Created by Luis Ascorbe on 07/05/2020.
+//  Copyright © 2020 Luis Ascorbe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Storage.
+FOUNDATION_EXPORT double StorageVersionNumber;
+
+//! Project version string for Storage.
+FOUNDATION_EXPORT const unsigned char StorageVersionString[];
