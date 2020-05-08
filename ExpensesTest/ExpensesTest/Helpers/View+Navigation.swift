169695fbaba03f266ac708818e1fa061a13dab7c
Luis Ascorbe <devlascorbe@gmail.com>
2020-05-08 12:16:34 +0200

    Add SwiftUI helper

---
+++ ExpensesTest/ExpensesTest/Helpers/View+Navigation.swift
@@ -0,0 +1,61 @@
+//
+//  View+Navigation.swift
+//  ExpensesTest
+//
+//  Created by Luis Ascorbe on 08/05/2020.
+//  Copyright © 2020 Luis Ascorbe. All rights reserved.
+//
+
+import SwiftUI
+
+extension View {
+    func withNavigation<T: View>(to destination: T) -> some View {
+        background(destination)
+    }
+}
+
+struct NavigationButton<CV: View, NV: View>: View {
+    @State private var isPresented = false
+    
+    var contentView: CV
+    var navigationView: (Binding<Bool>) -> NV
+    
+    var body: some View {
+        Button(action: {
+            self.isPresented = true
+        }) {
+            contentView
+                .withNavigation(to:
+                    navigationView($isPresented)
+                )
+        }
+    }
+}
+
+struct ModalLinkWrapper<T: View>: View {
+    typealias DestinationView = T
+    
+    var destination: T
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        EmptyView()
+            .sheet(isPresented: $isPresented, content: {
+                self.destination
+            })
+    }
+}
+
+struct NavigationLinkWrapper<T: View>: View {
+    typealias DestinationView = T
+    
+    var destination: T
+    @Binding var isPresented: Bool
+    var isDetailLink: Bool = true
+    
+    var body: some View {
+        NavigationLink(destination: destination, isActive: $isPresented) {
+            EmptyView()
+        }.isDetailLink(isDetailLink)
+    }
+}
